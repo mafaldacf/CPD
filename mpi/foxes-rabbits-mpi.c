e1aac5f2b523f30c33bb278524960495bd791c5d
Mafalda Ferreira <43276471+mafaldacf@users.noreply.github.com>
2022-10-01 00:58:55 +0100

    Add files via upload

---
+++ mpi/foxes-rabbits-mpi.c
@@ -0,0 +1,448 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <omp.h>
+#include <mpi.h>
+
+#define BLOCK_LOW(id,p,n) ((id)*(n)/(p))
+#define BLOCK_HIGH(id,p,n) (BLOCK_LOW((id)+1,p,n)-1)
+#define BLOCK_SIZE(id,p,n) (BLOCK_HIGH(id,p,n) - BLOCK_LOW(id,p,n) + 1)
+#define BLOCK_OWNER(index,p,n) (((p)*((index)+1)-1)/(n))
+
+#define ROCK '*'
+#define RABBIT 'R'
+#define FOX 'F'
+#define EMPTY ' '
+
+typedef struct Data {
+    char entity;
+    int breeding_age;
+    int starving_age;
+    int last_move_gen;
+    char current_entity;
+} data;
+
+typedef struct Position {
+    int i;
+    int j;
+} position;
+
+int id, p, first_line, last_line, lines, offset;
+uint32_t ngenerations, M, N, nrock, nrab, rbreed, nfox, fbreed, fstarv, seed;
+data **world;
+
+float r4_uni(uint32_t *seed) {
+    int seed_input, sseed;
+    float r;
+
+    seed_input = *seed;
+    *seed ^= (*seed << 13);
+    *seed ^= (*seed >> 17);
+    *seed ^= (*seed << 5);
+    sseed = *seed;
+    r = 0.5 + 0.2328306e-09 * (seed_input + sseed);
+
+    return r;
+}
+
+int data_empty(int i, int j) {
+    if(world[i][j].current_entity == EMPTY) return 1;
+    else return 0;
+}
+
+void insert_animal(int i, int j, char atype) {
+    world[i][j].current_entity = atype;
+    world[i][j].entity = atype;
+}
+
+int generate_element(int n, char atype, uint32_t *seed) {
+    int i, j, k, cnt = 0;
+
+    for(k = 0; k < n; k++) {
+        i = M * r4_uni(seed);
+        j = N * r4_uni(seed);
+        if((i >= first_line-1) && (i <= last_line+1)){
+            if(data_empty(i-offset, j)) {
+                insert_animal(i-offset, j, atype);
+                cnt++;
+            }
+        }
+    }
+
+    return cnt;
+}
+
+void reset_position(int i, int j) {
+    world[i][j].entity = EMPTY;
+    world[i][j].breeding_age = 0;
+    world[i][j].starving_age = 0;
+    world[i][j].last_move_gen = 0;
+    world[i][j].current_entity = EMPTY;
+}
+
+void conflict_resolution(data animal, int i_to, int j_to, int gen) {
+    switch(animal.entity) {
+        case RABBIT:
+
+            switch(world[i_to][j_to].entity) {
+                case EMPTY:
+                    world[i_to][j_to].entity = animal.entity;
+                    world[i_to][j_to].breeding_age = animal.breeding_age;
+                    world[i_to][j_to].last_move_gen = gen;
+                    break;
+                case RABBIT:
+                    if(animal.breeding_age > world[i_to][j_to].breeding_age)
+                        world[i_to][j_to].breeding_age = animal.breeding_age;
+                    break;
+                case FOX:
+                    world[i_to][j_to].starving_age = 0;
+                    break;
+            }
+            break;
+
+        case FOX:
+
+            switch(world[i_to][j_to].entity) {
+                case EMPTY:
+                    world[i_to][j_to].entity = animal.entity;
+                    world[i_to][j_to].breeding_age = animal.breeding_age;
+                    world[i_to][j_to].starving_age = animal.starving_age;
+                    world[i_to][j_to].last_move_gen = gen;
+                    break;
+                case RABBIT:
+                    world[i_to][j_to].entity = animal.entity;
+                    world[i_to][j_to].breeding_age = animal.breeding_age;
+                    world[i_to][j_to].starving_age = 0;
+                    world[i_to][j_to].last_move_gen = gen;
+                    break;
+                case FOX:
+                    if(animal.breeding_age > world[i_to][j_to].breeding_age) {
+                        world[i_to][j_to].breeding_age = animal.breeding_age;
+                        if(world[i_to][j_to].starving_age != 0)
+                            world[i_to][j_to].starving_age = animal.starving_age;
+                    }
+                    else if(animal.breeding_age == world[i_to][j_to].breeding_age) {
+                        if(animal.starving_age < world[i_to][j_to].starving_age)
+                            world[i_to][j_to].starving_age = animal.starving_age;
+                    }
+                    break;
+                }
+            break;
+    }
+}
+
+void move_rabbit(int i, int j, int gen) {
+    int cnt = 0, cell_number = ((i + offset) * N) + j, new_rabbit = 0;
+    position empty_neighbors[4], *p;
+
+    world[i][j].breeding_age++;
+    
+    if(i - 1 + offset >= 0 && world[i - 1][j].current_entity == EMPTY) {
+        empty_neighbors[cnt].i = i - 1;
+        empty_neighbors[cnt].j = j;
+        cnt++;
+    }
+    if(j + 1 < N && world[i][j + 1].current_entity == EMPTY) {
+        empty_neighbors[cnt].i = i;
+        empty_neighbors[cnt].j = j + 1;
+        cnt++;
+    }
+    if(i + 1 + offset < M && world[i + 1][j].current_entity == EMPTY) {
+        empty_neighbors[cnt].i = i + 1;
+        empty_neighbors[cnt].j = j;
+        cnt++;
+    }
+    if(j - 1 >= 0 && world[i][j - 1].current_entity == EMPTY) {
+        empty_neighbors[cnt].i = i;
+        empty_neighbors[cnt].j = j - 1;
+        cnt++;
+    }
+
+    if(cnt == 0) return;
+    p = &empty_neighbors[cell_number % cnt];
+
+    if(world[i][j].breeding_age >= rbreed) {
+        world[i][j].breeding_age = 0;
+        new_rabbit = 1;
+    }
+
+    // Conflict resolution
+    conflict_resolution(world[i][j], p->i, p->j, gen);
+
+    // Breeding or not
+    if(!new_rabbit) {
+        reset_position(i, j);
+    }
+}
+
+void move_fox(int i, int j, int gen) {
+    int empty_cnt = 0, rabbit_cnt = 0, cell_number = ((i + offset) * N) + j, new_fox = 0;
+    position empty_neighbors[4], rabbit_neighbors[4], *p;
+
+    world[i][j].breeding_age++;
+    world[i][j].starving_age++;
+
+    if(i - 1 + offset >= 0) {
+        if(world[i - 1][j].current_entity == EMPTY) {
+            empty_neighbors[empty_cnt].i = i - 1;
+            empty_neighbors[empty_cnt].j = j;
+            empty_cnt++;
+        }
+        else if(world[i - 1][j].current_entity == RABBIT) {
+            rabbit_neighbors[rabbit_cnt].i = i - 1;
+            rabbit_neighbors[rabbit_cnt].j = j;
+            rabbit_cnt++;
+        }
+    }
+    if(j + 1 < N) {
+        if(rabbit_cnt == 0 && world[i][j + 1].current_entity == EMPTY) {
+            empty_neighbors[empty_cnt].i = i;
+            empty_neighbors[empty_cnt].j = j + 1;
+            empty_cnt++;
+        }
+        else if(world[i][j + 1].current_entity == RABBIT) {
+            rabbit_neighbors[rabbit_cnt].i = i;
+            rabbit_neighbors[rabbit_cnt].j = j + 1;
+            rabbit_cnt++;
+        }
+    }
+    if(i + 1 + offset < M) {
+        if(rabbit_cnt == 0 && world[i + 1][j].current_entity == EMPTY) {
+            empty_neighbors[empty_cnt].i = i + 1;
+            empty_neighbors[empty_cnt].j = j;
+            empty_cnt++;
+        }
+        else if(world[i + 1][j].current_entity == RABBIT) {
+            rabbit_neighbors[rabbit_cnt].i = i + 1;
+            rabbit_neighbors[rabbit_cnt].j = j;
+            rabbit_cnt++;
+        }
+    }
+    if(j - 1 >= 0) {
+        if(rabbit_cnt == 0 && world[i][j - 1].current_entity == EMPTY) {
+            empty_neighbors[empty_cnt].i = i;
+            empty_neighbors[empty_cnt].j = j - 1;
+            empty_cnt++;
+        }
+        else if(world[i][j - 1].current_entity == RABBIT) {
+            rabbit_neighbors[rabbit_cnt].i = i;
+            rabbit_neighbors[rabbit_cnt].j = j - 1;
+            rabbit_cnt++;
+        }
+    }
+
+    if(rabbit_cnt > 0) {
+        p = &rabbit_neighbors[cell_number % rabbit_cnt];
+    }
+    else if(empty_cnt > 0) {
+        p = &empty_neighbors[cell_number % empty_cnt];
+    }
+    else{
+        return;
+    }
+
+    if(world[i][j].breeding_age >= fbreed) {
+        world[i][j].breeding_age = 0;
+        new_fox = 1;
+    }
+
+    // Conflict resolution
+    conflict_resolution(world[i][j], p->i, p->j, gen);
+
+    // Breeding or not
+    if(new_fox) {
+        world[i][j].starving_age = 0;
+    }
+    else {
+        reset_position(i, j);
+    }
+}
+
+void move(int i, int j, int gen) {
+
+    if(world[i][j].last_move_gen == gen) return;
+
+    switch(world[i][j].entity) {
+        
+        case RABBIT:
+            move_rabbit(i, j, gen);
+            break;
+        case FOX:
+            move_fox(i, j, gen);
+            break;
+    }
+}
+
+void communication(int gen, int color) {
+    int i;
+    data *ghost_line = (data *) malloc(N * sizeof(data));
+    MPI_Request request;
+    MPI_Status status;
+
+    // Send up (async)
+    // Receive from down
+    // Resolve conflicts
+    // Send back down (async)
+    // Receive from up
+    if(id != 0)
+        MPI_Isend(world[0], N * sizeof(data), MPI_BYTE, id - 1, 0, MPI_COMM_WORLD, &request);
+    if(id != p - 1) {
+        MPI_Recv(ghost_line, N * sizeof(data), MPI_BYTE, id + 1, 0, MPI_COMM_WORLD, &status);
+        for(i = (lines + offset + color) % 2; i < N; i += 2) {
+            if(ghost_line[i].last_move_gen == gen) conflict_resolution(ghost_line[i], lines, i, gen);
+        }
+        MPI_Isend(world[lines], N * sizeof(data), MPI_BYTE, id + 1, 1, MPI_COMM_WORLD, &request);
+    }
+    if(id != 0)
+        MPI_Recv(world[0], N * sizeof(data), MPI_BYTE, id - 1, 1, MPI_COMM_WORLD, &status);
+
+    // Send down (async)
+    // Receive from up
+    // Resolve conflicts
+    // Send back up (async)
+    // Receive from down
+    if(id != p - 1)
+        MPI_Isend(world[lines + 1], N * sizeof(data), MPI_BYTE, id + 1, 2, MPI_COMM_WORLD, &request);
+    if(id != 0) {
+        MPI_Recv(ghost_line, N * sizeof(data), MPI_BYTE, id - 1, 2, MPI_COMM_WORLD, &status);
+        for(i = (1 + offset + color) % 2; i < N; i += 2) {
+            if(ghost_line[i].last_move_gen == gen) conflict_resolution(ghost_line[i], 1, i, gen);
+        }
+        MPI_Isend(world[1], N * sizeof(data), MPI_BYTE, id - 1, 3, MPI_COMM_WORLD, &request);
+    }
+    if(id != p - 1)
+        MPI_Recv(world[lines + 1], N * sizeof(data), MPI_BYTE, id + 1, 3, MPI_COMM_WORLD, &status);
+}
+
+void run_simulation() {
+    int i, j, gen;
+
+    for(gen = 1; gen <= ngenerations; gen++){
+
+        // RED SUB-GENERATION
+
+        for(i = 1; i <= lines; i++){
+            for(j = (i + offset) % 2; j < N; j += 2){
+                move(i, j, gen);
+            }
+        }
+        
+        communication(gen, 1);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        for(i = 0; i <= lines + 1; i++){
+            for(j = 0; j < N; j++){
+                world[i][j].current_entity = world[i][j].entity;
+            }
+        }
+
+        // BLACK SUB-GENERATION
+
+        for(i = 1; i <= lines; i++){
+            for(j = (i + offset + 1) % 2; j < N; j += 2){
+                move(i, j, gen);
+            }
+        }
+
+        communication(gen, 0);
+
+        MPI_Barrier(MPI_COMM_WORLD);
+
+        for(i = 0; i <= lines + 1; i++){
+            for(j = 0; j < N; j++){
+                world[i][j].current_entity = world[i][j].entity;
+                // Check for the death of the fox
+                if(world[i][j].entity == FOX && world[i][j].starving_age >= fstarv) {
+                    reset_position(i, j);
+                }
+            }
+        }
+    }
+}
+
+void print_result() {
+    int i, j, total_rab, total_fox, total_rock;
+    nrab = nfox = nrock = 0;
+
+    for(i = 1; i <= lines; i++){
+        for(j = 0; j < N; j++){
+            switch (world[i][j].current_entity)
+            {
+                case RABBIT:
+                    nrab++;
+                    break;
+                case FOX:
+                    nfox++;
+                    break;
+                case ROCK:
+                    nrock++;
+                    break;
+            }
+        }
+    }
+
+    MPI_Reduce(&nrab, &total_rab, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&nfox, &total_fox, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&nrock, &total_rock, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if(!id)
+        printf("%d %d %d\n", total_rock, total_rab, total_fox);
+}
+
+int main(int argc, char *argv[]) {
+    double exec_time;
+    int i, j;
+
+    MPI_Init (&argc, &argv);
+    MPI_Comm_rank (MPI_COMM_WORLD, &id);
+    MPI_Comm_size (MPI_COMM_WORLD, &p);
+
+    ngenerations = atoi(argv[1]);
+    M = atoi(argv[2]);
+    N = atoi(argv[3]);
+    nrock = atoi(argv[4]);
+    nrab = atoi(argv[5]);
+    rbreed = atoi(argv[6]);
+    nfox = atoi(argv[7]);
+    fbreed = atoi(argv[8]);
+    fstarv = atoi(argv[9]);
+    seed = atoi(argv[10]);
+
+    lines = BLOCK_SIZE(id, p, M);
+    first_line = BLOCK_LOW(id, p, M);
+    last_line = BLOCK_HIGH(id, p, M);
+    offset = first_line - 1;
+
+    world = (data **) malloc((lines + 2) * sizeof(data *));
+    for(i = 0; i < (lines + 2); i++){
+        world[i] = (data *) malloc(N * sizeof(data));
+        for(j = 0; j < N; j++){
+            reset_position(i, j);
+        }
+    }
+
+    nrock = generate_element(nrock, ROCK, &seed);
+    nrab = generate_element(nrab, RABBIT, &seed);
+    nfox = generate_element(nfox, FOX, &seed);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    exec_time = -omp_get_wtime();
+
+    run_simulation();
+
+    exec_time += omp_get_wtime();
+
+    fprintf(stderr, "%.1fs\n", exec_time);
+
+    print_result();
+
+    for(i = 0; i < (lines + 2); i++){
+        free(world[i]);
+    }
+    free(world);
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ serial/foxes-rabbits.c
@@ -0,0 +1,373 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <omp.h>
+
+#define ROCK '*'
+#define RABBIT 'R'
+#define FOX 'F'
+#define EMPTY ' '
+
+typedef struct Data {
+    char entity;
+    int breeding_age;
+    int starving_age;
+    int last_move_gen;
+    char current_entity;
+} data;
+
+typedef struct Position {
+    int i;
+    int j;
+} position;
+
+uint32_t ngenerations, M, N, nrock, nrab, rbreed, nfox, fbreed, fstarv, seed;
+data **world;
+
+// 1st optimization
+int cp_index = 0;
+position *changed_positions = NULL;
+int df_index = 0;
+position *dead_foxes = NULL;
+
+float r4_uni(uint32_t *seed) {
+    int seed_input, sseed;
+    float r;
+
+    seed_input = *seed;
+    *seed ^= (*seed << 13);
+    *seed ^= (*seed >> 17);
+    *seed ^= (*seed << 5);
+    sseed = *seed;
+    r = 0.5 + 0.2328306e-09 * (seed_input + sseed);
+
+    return r;
+}
+
+int data_empty(int i, int j) {
+    if(world[i][j].current_entity == EMPTY) return 1;
+    else return 0;
+}
+
+void insertNode(position *head, int i, int j, int index) {
+    head[index].i = i;
+    head[index].j = j;
+}
+
+void insert_animal(int i, int j, char atype) {
+    world[i][j].entity = atype;
+    world[i][j].current_entity = atype;
+}
+
+int generate_element(int n, char atype, uint32_t *seed) {
+    int i, j, k, cnt = 0;
+
+    for(k = 0; k < n; k++) {
+        i = M * r4_uni(seed);
+        j = N * r4_uni(seed);
+        if(data_empty(i, j)) {
+            insert_animal(i, j, atype);
+            cnt++;
+        }
+    }
+
+    return cnt;
+}
+
+void reset_position(int i, int j) {
+    world[i][j].entity = EMPTY;
+    world[i][j].breeding_age = 0;
+    world[i][j].starving_age = 0;
+    world[i][j].last_move_gen = 0;
+    world[i][j].current_entity = EMPTY;
+}
+
+void move_rabbit(int i, int j, int gen) {
+    int cnt = 0, cell_number = (i * N) + j, new_rabbit = 0;
+    position empty_neighbors[4], *p;
+
+    world[i][j].breeding_age++;
+    
+    if(i - 1 >= 0 && world[i - 1][j].current_entity == EMPTY) {
+        empty_neighbors[cnt].i = i - 1;
+        empty_neighbors[cnt].j = j;
+        cnt++;
+    }
+    if(j + 1 < N && world[i][j + 1].current_entity == EMPTY) {
+        empty_neighbors[cnt].i = i;
+        empty_neighbors[cnt].j = j + 1;
+        cnt++;
+    }
+    if(i + 1 < M && world[i + 1][j].current_entity == EMPTY) {
+        empty_neighbors[cnt].i = i + 1;
+        empty_neighbors[cnt].j = j;
+        cnt++;
+    }
+    if(j - 1 >= 0 && world[i][j - 1].current_entity == EMPTY) {
+        empty_neighbors[cnt].i = i;
+        empty_neighbors[cnt].j = j - 1;
+        cnt++;
+    }
+
+    if(cnt == 0) return;
+    p = &empty_neighbors[cell_number % cnt];
+
+    if(world[i][j].breeding_age >= rbreed) {
+        world[i][j].breeding_age = 0;
+        new_rabbit = 1;
+        nrab++;
+    }
+
+    // Conflict resolution
+    switch(world[p->i][p->j].entity) {
+        case EMPTY:
+            world[p->i][p->j].entity = world[i][j].entity;
+            world[p->i][p->j].breeding_age = world[i][j].breeding_age;
+            world[p->i][p->j].last_move_gen = gen;
+            insertNode(changed_positions, p->i, p->j, cp_index++);
+            break;
+        case RABBIT:
+            if(world[i][j].breeding_age > world[p->i][p->j].breeding_age)
+                world[p->i][p->j].breeding_age = world[i][j].breeding_age;
+            nrab--;
+            break;
+        case FOX:
+            world[p->i][p->j].starving_age = 0;
+            nrab--;
+            break;
+    }
+
+    // Breeding or not
+    if(!new_rabbit) {
+        reset_position(i, j);
+    }
+}
+
+void move_fox(int i, int j, int gen) {
+    int empty_cnt = 0, rabbit_cnt = 0, cell_number = (i * N) + j, new_fox = 0;
+    position empty_neighbors[4], rabbit_neighbors[4], *p;
+
+    world[i][j].breeding_age++;
+    world[i][j].starving_age++;
+
+    if(i - 1 >= 0) {
+        if(world[i - 1][j].current_entity == EMPTY) {
+            empty_neighbors[empty_cnt].i = i - 1;
+            empty_neighbors[empty_cnt].j = j;
+            empty_cnt++;
+        }
+        else if(world[i - 1][j].current_entity == RABBIT) {
+            rabbit_neighbors[rabbit_cnt].i = i - 1;
+            rabbit_neighbors[rabbit_cnt].j = j;
+            rabbit_cnt++;
+        }
+    }
+    if(j + 1 < N) {
+        if(rabbit_cnt == 0 && world[i][j + 1].current_entity == EMPTY) {
+            empty_neighbors[empty_cnt].i = i;
+            empty_neighbors[empty_cnt].j = j + 1;
+            empty_cnt++;
+        }
+        else if(world[i][j + 1].current_entity == RABBIT) {
+            rabbit_neighbors[rabbit_cnt].i = i;
+            rabbit_neighbors[rabbit_cnt].j = j + 1;
+            rabbit_cnt++;
+        }
+    }
+    if(i + 1 < M) {
+        if(rabbit_cnt == 0 && world[i + 1][j].current_entity == EMPTY) {
+            empty_neighbors[empty_cnt].i = i + 1;
+            empty_neighbors[empty_cnt].j = j;
+            empty_cnt++;
+        }
+        else if(world[i + 1][j].current_entity == RABBIT) {
+            rabbit_neighbors[rabbit_cnt].i = i + 1;
+            rabbit_neighbors[rabbit_cnt].j = j;
+            rabbit_cnt++;
+        }
+    }
+    if(j - 1 >= 0) {
+        if(rabbit_cnt == 0 && world[i][j - 1].current_entity == EMPTY) {
+            empty_neighbors[empty_cnt].i = i;
+            empty_neighbors[empty_cnt].j = j - 1;
+            empty_cnt++;
+        }
+        else if(world[i][j - 1].current_entity == RABBIT) {
+            rabbit_neighbors[rabbit_cnt].i = i;
+            rabbit_neighbors[rabbit_cnt].j = j - 1;
+            rabbit_cnt++;
+        }
+    }
+
+    if(rabbit_cnt > 0) {
+        p = &rabbit_neighbors[cell_number % rabbit_cnt];
+    }
+    else if(empty_cnt > 0) {
+        p = &empty_neighbors[cell_number % empty_cnt];
+    }
+    else{
+        if(world[i][j].starving_age >= fstarv)
+            insertNode(dead_foxes, i, j, df_index++);
+        return;
+    }
+
+    if(world[i][j].starving_age >= fstarv)
+        insertNode(dead_foxes, p->i, p->j, df_index++);
+
+    if(world[i][j].breeding_age >= fbreed) {
+        world[i][j].breeding_age = 0;
+        new_fox = 1;
+        nfox++;
+    }
+
+    // Conflict resolution
+    switch(world[p->i][p->j].entity) {
+        case EMPTY:
+            world[p->i][p->j].entity = world[i][j].entity;
+            world[p->i][p->j].breeding_age = world[i][j].breeding_age;
+            world[p->i][p->j].starving_age = world[i][j].starving_age;
+            world[p->i][p->j].last_move_gen = gen;
+            insertNode(changed_positions, p->i, p->j, cp_index++);
+            break;
+        case RABBIT:
+            world[p->i][p->j].entity = world[i][j].entity;
+            world[p->i][p->j].breeding_age = world[i][j].breeding_age;
+            world[p->i][p->j].starving_age = 0;
+            world[p->i][p->j].last_move_gen = gen;
+            nrab--;
+            insertNode(changed_positions, p->i, p->j, cp_index++);
+            break;
+        case FOX:
+            if(world[i][j].breeding_age > world[p->i][p->j].breeding_age) {
+                world[p->i][p->j].breeding_age = world[i][j].breeding_age;
+                if(world[p->i][p->j].starving_age != 0)
+                    world[p->i][p->j].starving_age = world[i][j].starving_age;
+            }
+            else if(world[i][j].breeding_age == world[p->i][p->j].breeding_age) {
+                if(world[i][j].starving_age < world[p->i][p->j].starving_age)
+                    world[p->i][p->j].starving_age = world[i][j].starving_age;
+            }
+            nfox--;
+            break;
+    }
+
+    // Breeding or not
+    if(new_fox) {
+        world[i][j].starving_age = 0;
+    }
+    else {
+        reset_position(i, j);
+    }
+}
+
+void move(int i, int j, int gen) {
+
+    if(world[i][j].last_move_gen == gen) return;
+
+    switch(world[i][j].entity) {
+        
+        case RABBIT:
+            move_rabbit(i, j, gen);
+            break;
+        case FOX:
+            move_fox(i, j, gen);
+            break;
+    }
+}
+
+void run_simulation() {
+    int i, j, gen;
+    position *p;
+
+    for(gen = 1; gen <= ngenerations; gen++){
+
+        // RED SUB-GENERATION
+
+        for(i = 0; i < M; i++){
+            for(j = i % 2; j < N; j += 2){
+                move(i, j, gen);
+            }
+        }
+
+        for(i = 0; i < cp_index; i++) {
+            p = &changed_positions[i];
+            world[p->i][p->j].current_entity = world[p->i][p->j].entity;
+        }
+        cp_index = 0;
+
+        // BLACK SUB-GENERATION
+
+        for(i = 0; i < M; i++){
+            for(j = (i + 1) % 2; j < N; j += 2){
+                move(i, j, gen);
+            }
+        }
+
+        for(i = 0; i < cp_index; i++) {
+            p = &changed_positions[i];
+            world[p->i][p->j].current_entity = world[p->i][p->j].entity;
+        }
+        cp_index = 0;
+
+        for(i = 0; i < df_index; i++) {
+            p = &dead_foxes[i];
+            if(world[p->i][p->j].starving_age >= fstarv) {
+                reset_position(p->i, p->j);
+                nfox--;
+            }
+        }
+        df_index = 0;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    double exec_time;
+    int i, j, max_animals;
+
+    ngenerations = atoi(argv[1]);
+    M = atoi(argv[2]);
+    N = atoi(argv[3]);
+    nrock = atoi(argv[4]);
+    nrab = atoi(argv[5]);
+    rbreed = atoi(argv[6]);
+    nfox = atoi(argv[7]);
+    fbreed = atoi(argv[8]);
+    fstarv = atoi(argv[9]);
+    seed = atoi(argv[10]);
+
+    world = (data **) malloc(M * sizeof(data *));
+    for(i = 0; i < M; i++){
+        world[i] = (data *) malloc(N * sizeof(data));
+        for(j = 0; j < N; j++){
+            reset_position(i, j);
+        }
+    }
+
+    max_animals = (M * N) - nrock;
+    changed_positions = (position *) malloc(max_animals * sizeof(position));
+    dead_foxes = (position *) malloc(max_animals * sizeof(position));
+
+    nrock = generate_element(nrock, ROCK, &seed);
+    nrab = generate_element(nrab, RABBIT, &seed);
+    nfox = generate_element(nfox, FOX, &seed);
+
+    exec_time = -omp_get_wtime();
+
+    run_simulation();
+
+    exec_time += omp_get_wtime();
+
+    fprintf(stderr, "%.1fs\n", exec_time);
+
+    printf("%d %d %d\n", nrock, nrab, nfox);
+
+    for(i = 0; i < M; i++){
+        free(world[i]);
+    }
+    free(world);
+
+    free(changed_positions);
+    free(dead_foxes);
+
+    return 0;
+}
